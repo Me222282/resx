4216676f6d04e6838ea7ffd064c8b50cb80024ba
Zene <oinkymail3@gmail.com>
2023-08-06 19:33:13 +0100

    Creating .resx files

--- src/Program.cs
+++ src/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections;
+using System.IO;
+using System.Resources;
 
 namespace resx
 {
@@ -6,7 +9,134 @@ namespace resx
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            if (args.Length == 1 && args[0] == "--help")
+            {
+                // HELP
+                return;
+            }
+            
+            if (args.Length < 2)
+            {
+                Console.WriteLine("Invalid arguments.");
+                return;
+            }
+            
+            string file = args[1];
+            
+            if (args[0] == "new")
+            {
+                CreateResx(file);
+                return;
+            }
+            
+            if (args[0] == "add")
+            {
+                AddToResx(file);
+                return;
+            }
+            
+            Console.WriteLine($"Invalid argument {args[0]}.");
+        }
+        
+        private static void CreateResx(string path)
+        {
+            ResXResourceWriter rrw = new ResXResourceWriter(new FileStream(path, FileMode.Create));
+            
+            while (true)
+            {
+                string type;
+                while (true)
+                {
+                    Console.Write("Resource type (str/txt/bin): ");
+                    ConsoleKeyInfo cki = Console.ReadKey();
+                    // Exit
+                    if (cki.Key == ConsoleKey.Q &&
+                        cki.Modifiers == ConsoleModifiers.Control)
+                    {
+                        rrw.Close();
+                        ExitInfo(path);
+                        return;
+                    }
+                    
+                    type = cki.KeyChar + Console.ReadLine().ToLower().Trim();
+                    if (type != "str" && type != "txt" && type != "bin")
+                    {
+                        Console.WriteLine($"Invalid type.");
+                        continue;
+                    }
+                    break;
+                }
+                
+                Console.Write("Resource name: ");
+                string name = Console.ReadLine();
+                if (type == "str")
+                {
+                    Console.Write("Resource value: ");
+                    string value = Console.ReadLine();
+                    
+                    rrw.AddResource(name, value);
+                    continue;
+                }
+                
+                while (true)
+                {
+                    Console.Write("Resource path: ");                
+                    string rPath = Console.ReadLine();
+                    
+                    if (!File.Exists(rPath))
+                    {
+                        Console.WriteLine("File does not exist.");
+                        continue;
+                    }
+                    
+                    rrw.AddResource(name,
+                        new ResXDataNode(name,
+                            new ResXFileRef
+                            (
+                                rPath,
+                                type == "txt" ? typeof(string).FullName : typeof(byte[]).FullName
+                            )
+                        )
+                    );
+                    break;
+                }
+            }
+        }
+        
+        private static void AddToResx(string path)
+        {
+            if (!File.Exists(path))
+            {
+                Console.WriteLine($"File {path} does not exist.");
+                return;
+            }
+            
+            ResXResourceReader rrr = new ResXResourceReader(new FileStream(path, FileMode.Open));
+            
+            
+        }
+        
+        private static void ExitInfo(string path)
+        {
+            string name = Path.GetFileName(path);
+            string file = name.Remove(name.Length - Path.GetExtension(path).Length);
+            string dir = Path.GetRelativePath(Path.GetFullPath(path), Directory.GetCurrentDirectory());
+            
+            Console.WriteLine(@$"
+<ItemGroup>
+  <Compile Update=""{dir}/{file}.Designer.cs"">
+    <DesignTime>True</DesignTime>
+    <AutoGen>True</AutoGen>
+    <DependentUpon>{name}</DependentUpon>
+  </Compile>
+</ItemGroup>
+
+<ItemGroup>
+  <EmbeddedResource Update=""{dir}/{name}"">
+    <Generator>PublicResXFileCodeGenerator</Generator>
+    <LastGenOutput>{file}.Designer.cs</LastGenOutput>
+  </EmbeddedResource>
+</ItemGroup>");
         }
     }
 }
---
+++ src/Resources/SR.cs
@@ -0,0 +1,18 @@
+namespace System.Resources
+{
+    internal static class SR
+    {
+        public static string ResXResourceWriterSaved => "STUPID MICROSOFT CANNOT DO ANYTHING PROPERLY";
+        public static string InvalidResXBasePathOperation => "STUPID MICROSOFT CANNOT DO ANYTHING PROPERLY";
+        public static string SerializationException => "STUPID MICROSOFT CANNOT DO ANYTHING PROPERLY";
+        public static string InvocationException => "STUPID MICROSOFT CANNOT DO ANYTHING PROPERLY";
+        public static string InvalidResXFile => "STUPID MICROSOFT CANNOT DO ANYTHING PROPERLY";
+        public static string InvalidResXFileReaderWriterTypes => "STUPID MICROSOFT CANNOT DO ANYTHING PROPERLY";
+        public static string InvalidResXResourceNoName => "STUPID MICROSOFT CANNOT DO ANYTHING PROPERLY";
+        public static string InvalidResXNoType => "STUPID MICROSOFT CANNOT DO ANYTHING PROPERLY";
+        public static string NotSerializableType => "STUPID MICROSOFT CANNOT DO ANYTHING PROPERLY";
+        public static string TypeLoadException => "STUPID MICROSOFT CANNOT DO ANYTHING PROPERLY";
+        public static string NotSupported => "STUPID MICROSOFT CANNOT DO ANYTHING PROPERLY";
+        public static string TypeLoadExceptionShort => "STUPID MICROSOFT CANNOT DO ANYTHING PROPERLY";
+    }
+}
