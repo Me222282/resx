968c81a31771ac60228c43198323c47c662256c5
Zene <oinkymail3@gmail.com>
2023-08-06 17:23:21 +0100

    Creation

---
+++ src/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace resx
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
